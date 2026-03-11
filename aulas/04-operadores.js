6aa8b99dec0faf4b70e8a545129f9fba852fa81e
leoonavas <leoohnavas@gmail.com>
2026-03-11 16:58:11 -0300

    Add arithmetic operations and string concatenation examples

---
+++ aulas/04-operadores.js
@@ -0,0 +1,17 @@
+let x = 22
+let y = 6
+
+console.log("x =", x, "| y =", y);
+
+let result = x + y - 3;
+console.log(result);
+
+result = x * y / 2;
+console.log(result);
+
+console.log("X é igual a Y?", x == y);
+console.log("X é diferente de Y?", x != y);
+console.log("X é maior que Y?", x > y);
+console.log("X é menor que Y?", x < y);
+console.log("X é maior ou igual a Y?", x >= y);
+console.log("X é menor ou igual a Y?", x <= y);
