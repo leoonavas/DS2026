be0c4d819acd1895659bcdc083f0119e63685612
leoonavas <leoohnavas@gmail.com>
2026-03-11 11:13:19 -0300

    Add arithmetic operations for floating-point numbers and update variable values

--- aulas/03-operadores.js
+++ aulas/03-operadores.js
@@ -26,3 +26,32 @@ console.log("A divisão de x / y é", divisao); //mostrei o valor da soma
 
 //somando direto no console.log
 console.log("Somando números de 1 a 5 =", 1 + 2 + 3 + 4 + 5);
+
+// Operadores aritméticos
+
+let v1 = 12.5;
+let v2 = 4.7;
+
+var lines = "\n-------------------------------------\n";
+console.log(lines);
+console.log(`Valores: v1 = ${v1} e v2 = ${v2}`);
+console.log(lines);
+console.log(`Soma: v1 + v2 = ${v1 + v2}`);
+console.log(`Subtração: v1 - v2 = ${v1 - v2}`);
+console.log(`Multiplicação: v1 * v2 = ${v1 * v2}`);
+console.log(`Divisão: v1 / v2 = ${v1 / v2}`);
+console.log(lines);
+
+// alterando o value
+v1 = 15;
+
+console.log("\nRepetindo\n\n");
+
+console.log(lines);
+console.log(`Valores: v1 = ${v1} e v2 = ${v2}`);
+console.log(lines);
+console.log(`Soma: v1 + v2 = ${v1 + v2}`);
+console.log(`Subtração: v1 - v2 = ${v1 - v2}`);
+console.log(`Multiplicação: v1 * v2 = ${v1 * v2}`);
+console.log(`Divisão: v1 / v2 = ${v1 / v2}`);
+console.log(lines);
