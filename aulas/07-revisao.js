0a8e2f34f8b369b1837ff8ff635bba16be18e0e4
leoonavas <leoohnavas@gmail.com>
2026-03-17 09:27:10 -0300

    Add initial JavaScript file with comments and variable declarations

---
+++ aulas/07-revisao.js
@@ -0,0 +1,10 @@
+// comentario
+
+/*
+kelly
+*/
+
+console.log("sla");
+
+var nome = "Kelly"; // var é uma variavel global, utilizada no código todo, como o seu nome
+let apelido = "kellao"; // let é uma variavel local, utilizada somente no bloco onde foi declarada, como o seu apelido
